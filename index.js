5fcfb8088561223263b2f1531957d24c0af173c4
PRATEEK <son.prtk03@gmail.com>
2023-06-14 23:31:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,38 +2,6 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 const replaceTemplate = require("./modules/replaceTemplate");
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////  FILES
-
-//Blocking synchronous way
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
-
-// const textOut = `This is what we know about the avacado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync(`./txt/output.txt`, textOut);
-// console.log(`File written!`);
-
-//Non-Blocking asynchronous way
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//     if (err) return console.log("ERROR! 👺");
-
-//     fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//         console.log(data2);
-//         fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-//             console.log(data3);
-
-//             fs.writeFile(
-//                 "./txt/final.txt",
-//                 `${data2}\n${data3}`,
-//                 "utf-8",
-//                 (err) => {
-//                     console.log("Your file has been written 😁");
-//                 }
-//             );
-//         });
-//     });
-// });
-// console.log("Will read file");
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////  SERVER
